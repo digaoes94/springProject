453149e38400e105bffaa085112d71edb0dd352f
Rodrigo Araujo <rasch.dev@gmail.com>
2025-04-01 04:02:54 -0300

    Created UserService and updated UserResource

---
+++ src/main/java/com/educandoweb/springProject/services/UserService.java
@@ -0,0 +1,25 @@
+package com.educandoweb.springProject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.springProject.entities.User;
+import com.educandoweb.springProject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepo;
+	
+	public List<User> findAll() {
+		return userRepo.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepo.findById(id);
+		return obj.get();
+	}
+}
