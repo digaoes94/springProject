526dc9ca74aa42fab28ae1958c831689e83f0afc
Rodrigo Araujo <rasch.dev@gmail.com>
2025-04-23 03:06:09 -0300

    Created getSubTotal() at OrderItem and getTotal() at Order

--- src/main/java/com/educandoweb/springProject/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/springProject/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.educandoweb.springProject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.springProject.entities.OrderItem;
+import com.educandoweb.springProject.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
