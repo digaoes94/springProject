41c0d117c1c55c48644f687d9c6a99ee41dfc3aa
Rodrigo Araujo <rasch.dev@gmail.com>
2025-04-28 21:07:58 -0300

    Created insert() at UserService and UserResource

--- src/main/java/com/educandoweb/springProject/resources/UserResource.java
+++ src/main/java/com/educandoweb/springProject/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.springProject.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.springProject.entities.User;
 import com.educandoweb.springProject.services.UserService;
@@ -28,5 +32,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = userServ.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userServ.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
