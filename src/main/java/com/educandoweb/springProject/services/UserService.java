a4d583847794ce959969a8f94bbfe3d81ad72650
Rodrigo Araujo <rasch.dev@gmail.com>
2025-04-28 22:38:18 -0300

    Created update() at UserService and UserResource

--- src/main/java/com/educandoweb/springProject/services/UserService.java
+++ src/main/java/com/educandoweb/springProject/services/UserService.java
@@ -30,4 +30,16 @@ public class UserService {
 	public void delete(Long id) {
 		userRepo.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepo.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepo.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
