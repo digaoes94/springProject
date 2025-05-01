672078340ee64d0abb59c395cf63bf3da0229c0a
Rodrigo Araujo <rasch.dev@gmail.com>
2025-05-01 17:49:56 -0300

    Created ResourceNotFoundException, StandardError and ResourceExceptionHandler. Updated UserService.

--- src/main/java/com/educandoweb/springProject/services/UserService.java
+++ src/main/java/com/educandoweb/springProject/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.springProject.entities.User;
 import com.educandoweb.springProject.repositories.UserRepository;
+import com.educandoweb.springProject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepo.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
