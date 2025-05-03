26d8051b1eb6dfb20530f8213e0c3b2691c5ed79
Rodrigo Araujo <rasch.dev@gmail.com>
2025-05-03 02:38:13 -0300

    Created DatabaseException, updated delete::UserService and ResourceExceptionHandler

--- src/main/java/com/educandoweb/springProject/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/springProject/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.springProject.services.exceptions.DatabaseException;
 import com.educandoweb.springProject.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +18,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError sError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(sError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError sError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(sError);
 	}
